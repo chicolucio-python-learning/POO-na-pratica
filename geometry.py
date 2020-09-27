f07b37b4767f0bd691983173fd7434f1c843e691
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-09-26 22:33:36 -0300

    geometry - testes iniciais passando

--- geometry.py
+++ geometry.py
@@ -6,8 +6,15 @@ class Point:
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
 
 class Rect:
     def __init__(self, topLeft, botRight):
         self.topLeft = topLeft
         self.botRight = botRight
+
+    def center(self):
+        return Point((self.topLeft.x + self.botRight.x) / 2,
+                     (self.topLeft.y + self.botRight.y) / 2)
