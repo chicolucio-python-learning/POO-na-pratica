9a82a9120c27292f60cac9c1cd904b9e4a46a9b9
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-09-27 11:58:28 -0300

    integers - conjunto de testes Int16

--- test_integers.py
+++ test_integers.py
@@ -14,4 +14,16 @@ def test_int8():
     assert Int8(1) + Int8(1) == Int8(2)
     assert Int8(254) + Int8(1) == Int8(255)
 
-    assert Int8(127) + Int8(2) == Int8(254)
+    assert Int8(127) * Int8(2) == Int8(254)
+
+def test_int16():
+    assert isinstance(Int16(0), Int16)
+    assert isinstance(Int16(65535), Int16)
+
+    with pytest.raises(ValueError):
+        Int16(65536)
+
+    assert Int16(0) + Int16(0) == Int16(0)
+    assert Int16(255) + Int16(1) == Int16(256)
+
+    assert Int16(256) * Int16(2) == Int16(512)
