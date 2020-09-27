d7e6325c5d4e22b3ed13e54525ab45e2c1d8ad91
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-09-27 12:38:34 -0300

    integers - refatoração transformando select num factory interno como método de classe

--- integers.py
+++ integers.py
@@ -3,37 +3,34 @@ from abc import ABC
 from binary import Byte, adder, multiplier, Word, Tribyte, DoubleWord
 
 
-def select(n):
-    if 0 <= n < 256:
-        return Int8(n)
-    elif 256 <= n < 65_536:
-        return Int16(n)
-    elif 65_536 <= n < 16_777_216:
-        return Int24(n)
-    elif 16_277_216 <= n < 4_294_967_296:
-        return  Int32(n)
-    raise ValueError
-
-
 class Integer(ABC):
     STORAGE = bytes
     def __init__(self,n):
         self.value = self.STORAGE(n)
 
     def __add__(self, other):
-        sum_ = adder(self.value, other.value)
-        return select(sum_)
+        return self.factory(adder(self.value, other.value))
 
     def __eq__(self, other):
         return self.value == other.value
 
     def __mul__(self, other):
-        mul_ = multiplier(self.value, other.value)
-        return select(mul_)
+        return self.factory(multiplier(self.value, other.value))
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.value!r})'
 
+    @classmethod
+    def factory(cls, n):
+        if n < 256:
+            klass = Int8
+        elif n < 65_536:
+            klass = Int16
+        elif n < 16_777_216:
+            klass = Int24
+        elif n < 4_294_967_296:
+            klass = Int32
+        return klass(n)
 
 class Int8(Integer):
     STORAGE = Byte
