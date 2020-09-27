e156c0126a5839b34913f271ab311210663aae33
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-09-26 21:40:52 -0300

    geometry - testes iniciais que definem o programa

---
+++ test_geometry.py
@@ -0,0 +1,13 @@
+def test_rect():
+    r = Rect(Point(1, 1), Point(3, 3))
+    assert isinstance(r, Rect)
+    assert r.topLeft == Point(1, 1)
+    assert r.botRight == Point(3, 3)
+    assert r.center() == Point(2, 2)
+
+
+def test_point():
+    p = Point(2, 4)
+    assert isinstance(p, Point)
+    assert p.x == 2
+    assert p.y == 4
