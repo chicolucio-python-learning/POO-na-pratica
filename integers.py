b825fd2b7a12d0e24d35c829b176997d0e4eddd9
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-09-27 11:24:54 -0300

    integers - implementando soma e equivalência

--- integers.py
+++ integers.py
@@ -1,6 +1,13 @@
-from binary import Byte
+from binary import Byte, adder
 
 
 class Int8:
     def __init__(self, n):
         self.value = Byte(n)
+
+    def __add__(self, other):
+        sum_ = adder(self.value, other.value)
+        return Int8(sum_)
+
+    def __eq__(self, other):
+        return self.value == other.value
